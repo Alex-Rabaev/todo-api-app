ebc8a2626d43fe6ad0b5b5d02df0d4393b6737d6
Alex-Rabaev <rabaevav@live.com>
2025-07-18 17:40:51 +0300

    add: added TaskModel - create, update, delete and get list

---
+++ src/models/taskModel.ts
@@ -0,0 +1,29 @@
+import { db } from "../db";
+import { tasks } from "../schema/tasks";
+import { eq } from "drizzle-orm";
+
+export class TaskModel {
+  static async create(text: string, status: "in progress" | "completed") {
+    const inserted = await db.insert(tasks).values({ text, status }).returning();
+    return inserted[0];
+  }
+
+  static async updateStatus(id: number, status: "in progress" | "completed") {
+    const result = await db.update(tasks)
+      .set({ status })
+      .where(eq(tasks.id, id));
+    return result.changes;
+  }
+
+  static async delete(id: number) {
+    const result = await db.delete(tasks).where(eq(tasks.id, id));
+    return result.changes;
+  }
+
+  static async getTasksByStatusOrAll(status?: "in progress" | "completed") {
+    if (status) {
+      return await db.select().from(tasks).where(eq(tasks.status, status));
+    }
+    return await db.select().from(tasks);
+  }
+}
